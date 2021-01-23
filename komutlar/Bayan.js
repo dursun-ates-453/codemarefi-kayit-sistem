108fa4c24a62b723cd3b56efe5b92bf79151fd32
Kadir <58090942+KadirFi@users.noreply.github.com>
2021-01-23 13:01:54 +0300

    Create Bayan.js

---
+++ komutlar/Bayan.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+
+    exports.run = (client, message, args) => {
+        let user = message.mentions.members.first();
+        let isim = args[1];
+        let yas = args[2];
+
+        if(!user){
+            message.channel.send(`${message.author} **Lütfen Kullanıcı Belirt**`)
+        }
+        if(!isim){
+            message.channel.send(`${message.author} **İsim Girmeyi Unuttun**`)
+        }
+        if(!yas){
+            message.channel.send(`${message.author} **Yaş Girmen Zorunlu**`)
+        }
+
+        if(user && isim && yas){
+
+            let kadın = db.get(`kadınpuan_${message.author.id}_${message.guild.id}`) 
+            db.add(`kadınpuan_${message.author.id}_${message.guild.id}`, +1)
+
+            const kayit = new Discord.MessageEmbed()
+                .setColor('BLACK')
+                .setDescription(` ${user} **Kişisi,** <@&Kadın Rol İD> **Olarak Kayıt Edildi\n Toplam Kayıt Sayın: ${kadın || 0}**`)
+                .setFooter('CodeMareFi');
+            message.channel.send(kayit)
+
+            // Burda Yapacağımız İşlem Botu Kastırmasın Diye Belirli Bir Süre Vericez Rolleri Vermesi İçin
+            setTimeout(() => {
+                user.roles.add('KADIN ROL İD');
+                user.roles.remove('KAYITSIZ ROL İD');
+                user.setNickname(isim + " " + yas)
+            }, 2000) //2000 = 2 Saniye
+        }
+    }
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['Bayan','B','bayan','BAYAN','girl','Girl','GİRL'],
+    permLevel: 0
+}
+
+exports.help = {
+    name: 'b',
+    description: 'CodeMareFi V12 Gelişmiş Kayıt Sistemi',
+    usage: '.b @user isim yaş'
+}
